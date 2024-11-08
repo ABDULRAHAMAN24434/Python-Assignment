7c498f37d725e346963fcfc57e4ee7536055d2ed
Abdulrahaman24434 <rahmon_nosiru@yahoo.com>
2024-11-08 09:57:38 +0100

    My first PLP Python Project.

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("What's your name? ")
+age = input("How old are you? ")
+location = input("Where do you reside? ")
+print(f"Hello {name}!, you are {age} years old and lives in {location}")
